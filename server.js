1041ed79cc7aed6c106b28431c372a6635231aa5
carmen lm <carmen.info.lm@gmail.com>
2024-06-03 20:05:08 +0000

    auth

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const res = require('express/lib/response.js');
 const session = require('express-session');
 const passport = require('passport');
 const app = express();
+const routes = require('./routes.js');
+const auth = require('./auth.js');
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -22,11 +24,22 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
 
-app.route('/').get((req, res) => {
-  res.render('index', { title: 'Hello', message: 'Please log in' });
+
+myDB(async client => {
+  const myDataBase = await client.db('database').collection('users');
+
+  routes(app, myDataBase);
+  auth(app, myDataBase);  
+  
+  // Be sure to add this...
+}).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('index', { title: e, message: 'Unable to connect to database', showLogin: true });
+  });
 });
 
 const PORT = process.env.PORT || 3000;
