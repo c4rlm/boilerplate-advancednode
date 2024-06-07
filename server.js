7d50b35b5f6202cf37b1645bb3ffdd6ea0b98475
carmen lm <carmen.info.lm@gmail.com>
2024-06-07 15:45:39 +0000

    all proyect finished

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ const routes = require('./routes.js');
 const auth = require('./auth.js');
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const passportSocketIo = require('passport.socketio');
+const MongoStore = require('connect-mongo')(session);
+const cookieParser = require('cookie-parser');
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({ url: URI });
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -26,6 +31,27 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+io.use(
+  passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    key: 'express.sid', // nombre de la cookie
+    secret: process.env.SESSION_SECRET,
+    store: store,
+    success: onAuthorizeSuccess,
+    fail: onAuthorizeFail
+  })
+);
+
+function onAuthorizeSuccess(data, accept) {
+  console.log('successful connection to socket.io');
+  accept(null, true);
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+  if (error) throw new Error(message);
+  console.log('failed connection to socket.io:', message);
+  accept(null, false);
+}
 
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
@@ -37,17 +63,30 @@ myDB(async client => {
   routes(app, myDataBase);
   auth(app, myDataBase); 
   let currentUsers = 0;
-  io.on('connection', socket => {
-    console.log('A user has connected');
-    ++currentUsers;
-    io.emit('user count', currentUsers);
-  });
-  socket.on('disconnect', () => {
-    console.log('A user has disconnected');
-    --currentUsers;
-    io.emit('user count', currentUsers);
-  });
   
+
+  io.on('connection', socket => {
+    console.log('user ' + socket.request.user.username + ' connected');
+    ++currentUsers;
+    io.emit('user', {
+	  username: socket.request.user.username,
+	  currentUsers,
+	  connected: true
+	});
+  socket.on('chat message', (message) => {
+    io.emit('chat message', { name: socket.request.user.name, message });
+  });
+
+    socket.on('disconnect', () => {
+      console.log('user ' + socket.request.user.username + ' disconnected');
+      --currentUsers;
+      io.emit('user', {
+		  username: socket.request.user.username,
+		  currentUsers,
+		  connected: false
+	  });
+    });
+  });
   // Be sure to add this...
 }).catch(e => {
   app.route('/').get((req, res) => {
